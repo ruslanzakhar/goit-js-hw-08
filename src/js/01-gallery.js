e1ca68b4397cce66818f343fc9db48179313225e
ruslanzakhar <zakharchuk_ruslan@icloud.com>
2023-04-06 23:28:41 +0300

    hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector(`.gallery`);
+
+
+// *створює картинки по шаблону
+function createCardMarkup (galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+        return `<li><a class="gallery__item" href ="${original}">
+        <img class ="gallery__img" src="${preview}"  
+        alt="${description}"></a></li>`;
+    }).join(` `);
+    return 
+}
+// *додає створені картинки до галереї
+const cardsMarkup = (createCardMarkup(galleryItems));
+galleryContainer.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+
+
+
+const lightbox = new SimpleLightbox (`.gallery a`,{
+	captionsData: `alt`,
+    captionPosition: `bottom`,
+    captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = `feedback-form-state`;
+const form = document.querySelector(`.feedback-form`);
 
+
+form.addEventListener(`input`, throttle(onInputChange, 500));
+form.addEventListener(`submit`, onBtnSubmitClick);
+
+checkStorageContent();
+
+function onInputChange(evt){
+const fedbackForm = {
+    email:form.email.value,
+    message:form.message.value
+}
+
+const fedbackFormJSON = JSON.stringify(fedbackForm);
+
+localStorage.setItem(STORAGE_KEY,  fedbackFormJSON);
+
+};
+
+
+function checkStorageContent (){
+const savedfedbackForm = localStorage.getItem(STORAGE_KEY);
+if(savedfedbackForm){
+}
+try {
+    const fedbackForm = JSON.parse(savedfedbackForm);
+    form.email.value = fedbackForm.email;
+    form.message.value = fedbackForm.message;
+  } catch (error) {
+    console.log(error.name); 
+    console.log(error.message); 
+  }
+
+};
+
+function onBtnSubmitClick(evt){
+    evt.preventDefault();
+    const savedfedbackForm = localStorage.getItem(STORAGE_KEY);
+    try {
+        const fedbackForm = JSON.parse(savedfedbackForm);
+       console.log(fedbackForm);
+      } catch (error) {
+        console.log(error.name); 
+        console.log(error.message); 
+      }
+      if (form.email.value === "" || form.message.value === "") {
+        alert ("Please fill in all the fields!");
+      }
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    
+
+};
